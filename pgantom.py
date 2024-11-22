50ee0e93ee0e65335017ca75ce9f99eca838d414
Evgenii-mor <morozov1god@gmail.com>
2024-11-23 00:47:31 +0300

    Add files via upload

---
+++ pgantom.py
@@ -0,0 +1,14 @@
+print("hello world"' me name evgenii' )
+print(12+89-1)
+'1st program'
+print(9**0.5*5)
+print(type(9**0.5*5))
+"2st program"
+print(99.99>99.988888 and 1000!=1001)
+"3st program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2>2*(2+2))
+"4st program"
+print(type(int(123.456  )))
+print (123.456*10)
