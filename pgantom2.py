b30a68670b53cab4e84cbee12f8e36bba82ad90b
Evgenii-mor <morozov1god@gmail.com>
2024-11-30 12:51:30 +0300

    Add files via upload

---
+++ pgantom2.py
@@ -0,0 +1,32 @@
+
+
+
+print(12+89-1)
+'1st program'
+print(9**0.5*5)
+print(type(9**0.5*5))
+"2st program"
+print(99.99>99.988888 and 1000!=1001)
+"3st program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+"4st program"
+'123.456'
+print(float('123.456' and 123.456*10 and int(1234.56) and 1234.0/10))
+
+
+
+
+
+
+
+
+
+#print("hello world"' me name evgenii' )
+#name1='Евгений,'
+#a=12
+#b=1.5
+#name='Pathon,'
+#c=b/a
+#print(name1,'название курса',name,'всего заданий-',a,',затрачено часов-',b,',всего потрачено времни-',c)
