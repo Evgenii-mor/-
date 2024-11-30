f24adbe0d513e3268a21951f0f5f1f45a2368045
Evgenii-mor <morozov1god@gmail.com>
2024-11-30 13:26:30 +0300

    Add files via upload

---
+++ module_1_5.ry.py
@@ -0,0 +1,10 @@
+
+
+imutable_var=(1 , 2 ,'hello')
+print(imutable_var)
+mutable_list=['banan' ,'apple']
+print(mutable_list)
+mutable_list.remove('apple')
+print(mutable_list)
+mutable_list.append('peach')
+print(mutable_list)
