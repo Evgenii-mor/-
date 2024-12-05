a8e22761007c24dea61b3f39a920e3bd091a3ca4
Evgenii-mor <morozov1god@gmail.com>
2024-12-05 18:39:21 +0300

    Add files via upload

--- module_1_7.ry.py
+++ module_1_7.ry.py
@@ -1,6 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+student = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades1 = zip(student, grades)
+grades = dict(grades1)
+print(grades)
+grades.keys()
+grades.items()
 
-print('Hi,PyCharm')
-x=43
-y=32
-print(x*y)
-print("End line")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
